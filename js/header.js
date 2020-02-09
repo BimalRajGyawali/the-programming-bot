8195d74d96e16139f0bc7bafa34ad40148438bbb
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2020-02-09 16:20:22 +0545

    Header added

---
+++ js/header.js
@@ -0,0 +1,7 @@
+var menu = document.getElementsByClassName('mobile-menu')[0];
+var nav = document.getElementById('nav');
+
+menu.addEventListener('click',function(){
+    nav.classList.toggle('hide');
+
+});
